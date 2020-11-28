e6b2cc8c271dc3afd4cd138ac61e54cf7d574995
Thomas Abbott <twabbott@ucdavis.edu>
2020-11-28 11:09:39 -0800

    Adding a Test file for the team!

---
+++ hello.py
@@ -0,0 +1,13 @@
+'''
+Hey Team, so we know who has added themselves to the repo
+please append your name to the teamNames array. To test 
+python is working on your computer you can run this script
+'''
+style = "\*/"*20
+teamNames = ['Thomas']
+
+for name in teamNames:
+    print("\n" + style)
+    print("Hello Rove Crest, {} Here!".format(name))
+    print("Just wanted to check that I could push to the repo!")
+    print(style)
