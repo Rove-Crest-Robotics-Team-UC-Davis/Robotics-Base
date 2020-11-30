8ff45199d04819bc0403d983d7fe4aecd67d3bd4
Adityaa Ravi <adityaaravi6@gmail.com>
2020-11-29 23:24:28 -0800

    ROS handler overall game plan added to the main.py file.

--- src/main.py
+++ src/main.py
@@ -2,4 +2,20 @@
 # This file is the master for ROS handler
 # importing libararies
 import rospy
+import methods.py
 
+# initialize a variable to store the last sent value
+
+# Create a ros publisher node 'reqd_pos' (that sends Strings)
+
+while True:
+    # Listen to IK's program's publishing node and store it in a local variable
+
+    # Listen to Ardunio's publishing node and store the string onto a variable.
+
+    # while the robot arm's position is not equal to last sent, wait.
+
+    # Send the message from IK to arduino
+
+    # Update the last sent variable.
+    pass  
