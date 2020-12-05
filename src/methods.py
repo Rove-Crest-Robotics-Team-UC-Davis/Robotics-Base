8bd06046b70045463c0bcd1afcb61a17949b5895
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-05 00:18:45 -0800

    fixed some more errors

--- src/methods.py
+++ src/methods.py
@@ -8,11 +8,11 @@ class sender:
     pub = None
     def __init__(self, send_to):
         # create a publisher to send messages to the given topic
-        pub = rospy.Publisher(send_to, String, queue_size=20)
+        self.pub = rospy.Publisher(send_to, String, queue_size=20)
     
     def send(self, message):
         # send the given message to the topic given in the constructor
-        pub.publish(message)
+        self.pub.publish(message)
         rospy.loginfo(message)
         
 
@@ -22,13 +22,13 @@ class reader:
     pub = None
     r_index = -1
     def set_reader_data(string):
-        data.reader_data[r_index] = string
+        data.reader_data[self.r_index] = string
 
     def __init__(self, get_from, reader_index):
-        r_index = reader_index
+        self.r_index = reader_index
         # create a listener to read messages from the given topic
-        pub = rospy.Subscriber(get_from, String, set_reader_data)
+        self.pub = rospy.Subscriber(get_from, String, self.set_reader_data)
 
     def get_next(self):
         # return the next command.
-        return data.reader_data[r_index]
+        return data.reader_data[self.r_index]
