45bb65604fc6bc79dbf538131b562536cf043e5a
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-05 00:15:52 -0800

    all inital code done, testing remains

--- src/methods.py
+++ src/methods.py
@@ -1,7 +1,7 @@
 # import libraries.
 import rospy
 from std_msgs.msg import String
-import static_data
+import static_data as data
 # http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
  
 class sender:
@@ -16,13 +16,19 @@ class sender:
         rospy.loginfo(message)
         
 
+
 class reader:
     # create object properties -- sender and reader
     pub = None
+    r_index = -1
+    def set_reader_data(string):
+        data.reader_data[r_index] = string
+
     def __init__(self, get_from, reader_index):
+        r_index = reader_index
         # create a listener to read messages from the given topic
-        pub = rospy.Subscriber(get_from, String, callback=None)
+        pub = rospy.Subscriber(get_from, String, set_reader_data)
 
     def get_next(self):
-        # return the next command from the aforementioned array.
-        return "SAMPLE STRING"
+        # return the next command.
+        return data.reader_data[r_index]
