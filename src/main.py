f42336d64743fadbebeb83c672f6ebf620d8ee23
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-04 22:51:49 -0800

    timely save -- main loop coding in progress

--- src/main.py
+++ src/main.py
@@ -9,13 +9,17 @@ from methods import *
 last_sent = ""
 # Create a ros publisher node 'reqd_pos' (that sends Strings)
 # queue size --> the number of messages that this topic must store as a backlog. 
-pub_node = handler.__init__('reqd_pos', ['curr_pos', 'IK_in'])
+reqd_pos = sender.__init__('reqd_pos')
+curr_pos = reader.__init__('curr_pos')
+ik_in = reader.__init__('ik_in')
 
-while True:
+while not rospy.is_shutdown():
     # Listen to IK's program's publishing node and store it in a local variable
-
+    from_ik = ik_in.get_next()
     # Listen to Ardunio's publishing node and store the string onto a variable.
-
+    from_ard = curr_pos.get_next()
+    while not (from_ard == from_ik):
+        from_ard = curr_pos.get_next()
     # while the robot arm's position is not equal to last sent, wait.
 
     # Send the message from IK to arduino
--- src/methods.py
+++ src/methods.py
@@ -9,10 +9,10 @@ class sender:
         # send the given message to the topic given in the constructor
         pass
 
-class handler:
+class reader:
     # create object properties -- sender and reader
 
-    def __init__(self, send_to, get_from):
+    def __init__(self, get_from):
         # create a publisher to send messages to the given topic
 
         # create a listener to read messages from the given topic
@@ -20,14 +20,6 @@ class handler:
         # initialize both of the above
         pass
 
-    
-
-    def create_reader(self):
-        # recieve data and add it to the array of commands from IK
-        pass
-    def get_command(self, index):
+    def get_next(self, index):
         # return the 'index'th command from the aforementioned array.
-        pass
-    def done_command(self, index):
-        # remove the command of the given index from the array.
         pass
