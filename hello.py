978e4635c4c42ce224fe3cf0e2d5d6eab1b60a51
kkrachenfels <kkrachenfels@ucdavis.edu>
2020-12-06 16:09:37 -0800

    Added name to hello.py file

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 '''
 Hey Team, so we know who has added themselves to the repo
-please append your name to the teamNames array. To test 
+please append your name to the teamNames array. To test
 python is working on your computer you can run this script
 '''
 style = "\*/"*20
-teamNames = ['Thomas','Adityaa']
+teamNames = ['Thomas','Adityaa', 'Kay']
 
 for name in teamNames:
     print("\n" + style)
